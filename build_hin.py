30edfd8e8866365d876572cb92d80b9eff652fbc
yigh <yizhou12345@gmail.com>
2014-09-10 20:48:23 -0500

    Metapath operations

--- build_hin.py
+++ build_hin.py
@@ -1,89 +1,101 @@
-class HIN:
-    def __init__(self, publications_file_path) :
-        input_file = open(publications_file_path)
-        self.papers = dict()
-        self.authors = dict()
-        self.venues = dict()
-        for line in input_file :
-            if line[:2] == "#*" :
-                self.add_new_paper(input_file)
+'''
+    Usage : 
+    from build_hin import start 
+    p_a, p_p, p_v, a_p, v_p = start()
+'''
 
-    def add_new_paper(self, input_file) :
-        new_paper = Paper()
-        line_count = 0
-        for line in input_file :
-            print "line:"+str(line_count)
-            line_count += 1
-            line = line.strip()
+'''
+    Define the one-to-many relations in network schema
+'''
+author_papers = dict()
+paper_papers = dict()
+paper_authors = dict()
+venue_papers = dict()
+
+'''
+    Define the one-to-one relations in network schema
+'''
+paper_venue = dict()
+
+def dictionary_add_set(dictionary, key, value) :
+    '''
+        Add key-value pair to dictionary, where the collection type is set.
+    '''
+    if not dictionary.has_key(key) :
+        dictionary[key] = set()
+    dictionary[key].add(value)
+
+def start() :
+
+    input_file = open("publications.txt")
+    while True :
+        '''
+            Parse paper title.
+            Test for EOF.
+        '''
+        line = input_file.readline().strip()
+        if len(line) == 0 :
+            break
+        assert line[:2] == "#*"
+
+        '''
+            Parse author.
+        '''
+        line = input_file.readline().strip()
+        assert line[:2] == "#@"
+        authors = line[2:].split(',')
+
+        '''
+            Parse Year
+        '''
+        input_file.readline()
+
+        '''
+            Parse Venue
+        '''
+        line = input_file.readline().strip()
+        assert line[:2] == "#c"
+        venue = line[2:]
+
+        '''
+            Parse paper id.
+            Do not cast to integer. Simply unnecessary.
+        '''
+        line = input_file.readline().strip()
+        assert line[:6] == "#index"
+        id = line[6:]
+
+        for a in authors :
+            dictionary_add_set(author_papers, a, id)
+        dictionary_add_set(venue_papers, venue, id)
+
+        paper_venue[id] = venue
+        paper_authors[id] = authors
+
+        '''
+            Parse citations.
+        '''
+        line = input_file.readline().strip()
+        while line[:2] == "#%" :
             '''
-                End of data for one paper. Verify all fields are set and then
-                add to the HIN database.
+                Invalid/empty citation.
             '''
-            if line[:2] == "#!" :
-                if new_paper.verify_fields_are_set() :
-                    self.papers[new_paper.id] = new_paper
+            if len(line) <= 2 :
+                break
+            dictionary_add_set(paper_papers, id, line[2:])
+            line = input_file.readline().strip()
 
-                    for author in new_paper.get_authors() :
-                        if not self.authors.has_key(author) :
-                            self.authors[author] = set()
-                        self.authors[author].add(new_paper)
+        '''
+            Read the empty string line so the readline output is not confused with
+            EOF.
+            Sets the reading pointer to the next paper's title line.
+        '''
+        line = input_file.readline()
+        if line[:2] == "#!" :
+            input_file.readline()
 
-                    if not self.venues.has_key(new_paper.get_venue()) :
-                        self.venues[new_paper.get_venue()] = set()
-                    self.venues[new_paper.get_venue()].add(new_paper)
-
-            elif line[:2] == "#c" :
-                new_paper.set_conference(line[2:])
-
-            elif line[:2] == "#@" :
-                new_paper.add_authors(line[2:].split(','))
-
-            elif line[:2] == "#t" :
-                new_paper.set_year(int(line[2:]))
-
-            elif line[:2] == "#%" :
-                if(len(line[2:]) > 0) :
-                    new_paper.add_reference(int(line[2:]))
-
-            elif line[:6] == "#index" :
-                new_paper.set_self_id(int(line[6:]))
-
-class Paper:
-    def __init__(self) :
-        self.reference_list = []
-        self.authors = []
-        self.year = None
-        self.conference = None
-        self.id = None
-
-    def add_reference(self, reference_id) :
-        self.reference_list.append(reference_id)
-
-    def set_year(self, year) :
-        self.year = year
-
-    def set_self_id(self, self_id) :
-        self.id = self_id
-
-    def add_authors(self, author_list) :
-        self.authors.extend(author_list)
-
-    def set_conference(self, conference) :
-        self.conference = conference
-
-    def get_venue(self) :
-        return self.conference
-
-    def get_authors(self) :
-        return self.authors
-
-    def verify_fields_are_set(self) :
-        if self.year == None or \
-           self.id == None or \
-           self.authors == [] or \
-           self.conference == None :
-                return False
-        return True
-
-print("Start")
-hin = HIN("publications.txt")
+    return paper_authors, \
+           paper_papers, \
+           paper_venue, \
+           author_papers, \
+           venue_papers
